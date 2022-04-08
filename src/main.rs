3ac2d2633d3e69d12e9471078c8cab9248879dc2
Justin Bradford <jabradford@gmail.com>
2022-04-08 00:41:12 -0500

    Initial sketch of the reactor/process code structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+trait Process {
+  fn rate(&self) -> f64;
+  fn perform(&self);
+}
+
+struct Reactor {
+  steps: u64,
+  time: f64,
+  processes: Vec<Box<dyn Process>>
+}
+
+impl Reactor {
+  fn new() -> Self {
+    Self {
+      steps: 0,
+      time: 0.0,
+      processes: Vec::new()
+    }
+  }
+
+  fn step(&mut self) {
+    let total_rate:f64 = self.processes.iter().map(|p| p.rate()).sum();
+    println!("{}", total_rate);
+    self.steps += 1;
+    self.time += 0.1; // todo
+    // self.choose().perform(); // todo
+  }
+}
+
+impl Process for f64 {
+  fn rate(&self) -> f64 { *self }
+  fn perform(&self) {}
+}
+
 fn main() {
-    println!("Hello World!");
+  let mut reactor = Reactor::new();
+  reactor.step();
+  reactor.processes.push(Box::new(44.5));
+  reactor.processes.push(Box::new(2.25));
+  reactor.step();
+  println!("Hello World!");
 }
